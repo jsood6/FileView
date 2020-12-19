a705898c2da1efc6d49e1b6f9943b8c9c90c2ba0
Jigyasaa Sood <jigyasaasood@Jigyasaas-MacBook-Pro.local>
2020-12-18 19:59:26 -0800

    Initial Commit

---
+++ FileView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileView
+//
+//  Created by Jigyasaa Sood on 12/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
